b2eb36b92173c15a69382fbff7f832419b6c6dc0
Marcel <khouri10@gmail.com>
2017-12-03 10:27:28 -0200

    adicionar solicitação do foursquare com lista dos 5 restaurantes próximos do local apontado

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//lista de lugares de interesse
+//lista inicial dos lugares de interesse
 var lugaresIniciais = [
 {
     nome: 'M10',
@@ -45,8 +45,9 @@ var marcadorDestacado;
 
 var marcadorSelecionado;
 
-var lugarAtual = 1
+var largeInfowindow;
 
+//função para fazer o marcador de acordo com a cor e tamanho solicitados (crédito aula do google maps api)
 function makeMarkerIcon(markerColor, markerWidth, markerHeight) {
         var markerImage = new google.maps.MarkerImage(
           'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
@@ -58,6 +59,21 @@ function makeMarkerIcon(markerColor, markerWidth, markerHeight) {
         return markerImage;
       }
 
+//função para fazer aparecer o info window (crédito aula do google maps api)      
+function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+        }
+      }
+
+
 var initMap = function() {
 	
 	//inicia o mapa na tela inteira
@@ -67,12 +83,14 @@ var initMap = function() {
     	mapTypeControl: false
     });
 
+	//criar 3 tipos de marcadores com cores e/ou tamanhos diferentes
 	marcadorPadrao = makeMarkerIcon('F44336', 21, 34);
-
 	marcadorDestacado = makeMarkerIcon('FFC107', 21, 34);
-
     marcadorSelecionado = makeMarkerIcon('4CAF50', 25, 41);
-    //coloca marcadores no mapa
+
+    largeInfowindow = new google.maps.InfoWindow();
+    
+    //coloca marcadores dos lugares iniciais no mapa
     for (var i = 0; i < lugaresIniciais.length; i++) {
     	var marcador = new google.maps.Marker({
           position: lugaresIniciais[i].posição,
@@ -82,9 +100,11 @@ var initMap = function() {
           icon: marcadorPadrao
         });
         
+        //olocaa infowindow em cada marcador ao ser clicado e destaca o icone
         marcador.addListener('click', function() {
             
             this.setIcon(marcadorSelecionado);
+            populateInfoWindow(this, largeInfowindow);
              
         });
   
@@ -102,7 +122,7 @@ var initMap = function() {
     }
 }
 
-
+//construtor para lista de lugares
 var Lugar = function(data){
     this.nome = ko.observable(data.nome);
     this.posição = ko.observable(data.posição);
@@ -112,34 +132,46 @@ var Lugar = function(data){
 
 }
 
+//construtor para lista de restaurantes
+var Restaurante = function(data){
+    this.nome = ko.observable(data.name);
+    this.lat = ko.observable(data.location.lat);
+    this.lng = ko.observable(data.location.lng);
+}
+
 var ViewModel = function() {
     var self = this;
 
-    //cria uma array observavel vazia
+    //cria uma array observavel vazia para ser preenchida com os lugares iniciais
     this.lugarLista = ko.observableArray([]);
-    this.lugarSelecionado = ko.observable(lugarAtual)
+  
 
     //coloca todos os dados iniciais na array osbservavel
     lugaresIniciais.forEach(function(lugarItem){
         self.lugarLista.push( new Lugar(lugarItem) );
     });
     
-    //função para selecionar o lugar
+    //função para destacar o lugar selecionado
     this.selecionarLugar = function(lugar){
+    	//deixa todos marcadores com aparencia padrão
         for (var i = 0; i < self.lugarLista().length; i++) {
            self.lugarLista()[i].marcador()[0].setIcon(marcadorPadrao);
         }
+        //destaca o marcador selecionado
         lugar.marcador()[0].setIcon(marcadorSelecionado);
+        $("#item").attr('class', 'item-selecionado');
+       
+
     }
 
     
     //cria um observavel para linkar com o evento da pesquisa    
     this.pesquisa = ko.observable(''); 
 
-    //função que esconde os itens que não correspondem a pesquisa
-    // e mostra todos quando estiver vazia
+    //função filtra os lugares e marcadores de acordo com a pesquisa
     this.pesquisar = function(value) {
     	
+    	//se a pesquisa estiver vazia mostra todos os lugares na lista e no mapa
         if (value == '') {
         	for (var i = 0; i < self.lugarLista().length; i++) {
         		self.lugarLista()[i].marcador()[0].setMap(map);
@@ -147,6 +179,7 @@ var ViewModel = function() {
         	}
         }
 
+        //se o item corresponde à pesquisa é mostrado, caso o contrário esconde o marcador no mapa e na lista 
         self.lugarLista().forEach(function(lugarItem){
             if (lugarItem.nome().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
                     lugarItem.marcador()[0].setMap(map);
@@ -160,8 +193,37 @@ var ViewModel = function() {
     };
 
     this.pesquisa.subscribe(self.pesquisar);
+    
+    //pega os paramatros para a solicitação (apenas do primeiro item a fins de teste)
+    var latitude = self.lugarLista()[0].posição().lat;
+    var longitude = self.lugarLista()[0].posição().lng;
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' + latitude + ',' + longitude +
+        '&client_id=RCU1XOXGZMYWAK0NX2YN2BXWCFH1V2VZSM4VB2GFKNJ1CIJT&client_secret=0VLNN01DE2OSEUYPXLYFJLA4Y0GVQDTVGXUPZRPR2D1JFZZS&v=20160118&query=restaurante&limit=5'
+    
+    this.restaurantes = ko.observableArray([]);
+
+    //solicitação da API do foursquare de pesquisa dos 5 restaurantes mais próximos do lugar apontado
+    $.getJSON(foursquareURL, function( data ) {
+        for (var i = 0; i < data.response.venues.length; i++) {
+        	var response = data.response.venues[i];
+        	self.restaurantes().push(new Restaurante(response));
+        	console.log(response.name) //verificação se tá certa a solicitação
+        }
+
+        	
+        
+
+    }).fail(function(){
+        console.log('erro');
+    });
+
+
+
+
 }
 
 ko.applyBindings(new ViewModel());
+
+
 
 
